b37775a4b94c3ef53f1817b5cb7ce565c31b5797
csisemore <camilla.sisemore@gmail.com>
2019-01-13 16:10:49 -0600

    Final without bonus missions.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -9,19 +9,81 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static org.launchcode.controllers.ListController.columnChoices;
+
 /**
  * Created by LaunchCode
  */
 @Controller
 @RequestMapping("search")
-public class SearchController {
+public class SearchController<jobResults> {
+
+    private String column;
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", columnChoices);
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    // TODO #1 - Create handler to process search request and display results DONE
 
+//    @RequestMapping(value = "results")
+//    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+//        //model.addAttribute("columns", columnChoices);
+//        //ArrayList<HashMap<String, String>> jobResults = new ArrayList<>();
+//        //ArrayList<HashMap<String, String>> jobResults;
+//        if (searchType.equals("all")) {
+//            ArrayList<HashMap<String, String>> jobResults = new ArrayList<>();
+//            String qtyOfResults = jobResults.size() + " Result(s)";
+//            jobResults = JobData.findByValue(searchTerm);
+//            model.addAttribute("title", searchTerm); //+ " from all columns");
+//            model.addAttribute("listItems", jobResults);
+//            model.addAttribute("columns", columnChoices);
+//            return "search";
+//        } else {
+//            ArrayList<HashMap<String, String>> jobResults = JobData.findByColumnAndValue(searchType, searchTerm);
+//            String qtyOfResults = jobResults.size() + " Result(s)";
+//            jobResults = JobData.findByColumnAndValue(searchType, searchTerm);
+//            model.addAttribute("title", searchTerm + " from " + searchType + " columns");
+//            model.addAttribute("listItems", jobResults);
+//            model.addAttribute("columns", columnChoices);
+//            return "search";
+//        }
+//    }
+//
+
+    @RequestMapping(value = "results")
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", columnChoices);
+
+
+        ArrayList<HashMap<String, String>> jobResults;
+
+        if ("all".equals(searchType)) {
+            jobResults = JobData.findByValue(searchTerm);
+        } else {
+            jobResults = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        //model.addAttribute("jobs", jobResults.size());
+        model.addAttribute("jobs", jobResults);
+        model.addAttribute("listItems", jobResults);
+        model.addAttribute("title", searchTerm + " from " + searchType + " columns");
+
+        return "search";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
